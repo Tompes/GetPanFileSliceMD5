e03de40e019218454ef43a5d30f0b6f686c242b2
li li <li li>
2018-05-19 21:00:17 +0800

    Upload code

---
+++ func.class.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ * Author Tompes
+ * Github https://github.com/Tompes/GetPanFileSliceMD5
+ */
+
+class GetSliceMD5 {
+    public $message;
+    public $token;
+    public function __construct($t=0){
+        $this->token = $t;
+    }
+
+
+    public function sliceMD5($link,$referrer=0,$size=262143,$ua=0){
+        //if you want get 10 byte slice form the whole file, you must import $size = 9
+
+        $slice = $this->getStream($link,0,$referrer,$size,$ua);
+        if(!$slice){
+            $this->message = "Failed to got slice,please check parameter out and try again!";
+            return false;
+        }
+        return md5($slice);
+    }
+    private function getStream($link,$post=0,$referrer=0,$size=0,$ua=0){
+        $ua       = $ua?$ua:$_SERVER['HTTP_USER_AGENT'];
+        $range    = $size?"Range: bytes=0-{$size}\r\n":"";
+        $referrer = $referrer?$referrer:"https://pan.baidu.com/disk/home";
+
+        $opts = [
+            "http" => [
+                "method" => $post?"POST":"GET",
+                "timeout"=>60,
+                "header" => "Accept-language: *\r\n" .
+                    $range.
+                    "Accept:*/*\r\n".
+                    "Accept-Encoding:gzip, deflate, sdch, brr\n".
+                    "Cache-Control:no-cache\r\n".
+                    "Connection:keep-alive\r\n".
+                    "Cookie:{$this->token}\r\n".
+                    "Referer:{$referrer}\r\n".
+                    "Pragma:no-cache\r\n".
+                    "User-Agent:{$ua}\r\n",
+                "content"=> $post?$post:"",
+            ]
+        ];
+
+        $context = stream_context_create($opts);
+
+// Open the file using the HTTP headers set above , then return it.
+
+        return @file_get_contents($link, false, $context);
+
+    }
+}
---
+++ index.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Author Tompes
+ * Github https://github.com/Tompes/GetPanFileSliceMD5
+ */
+
+require 'func.class.php';
+
+$s = new GetSliceMD5(); //instantiate
+
+if(isset($_GET['action'])){
+    $act = addslashes($_GET['action']);
+    if($act=="smd5"){
+        if(empty($_GET['link']))
+            die("{\"error\":\"-1\",\"msg\":\"parameter error.\"}");
+
+        $link = addslashes($_GET['link']);
+        $hash = $s->sliceMD5(urldecode($link));
+        if(!$hash)
+            die("{\"error\":\"-2\",\"msg\":\"{$s->message}\"}");
+
+        echo "{\"error\":\"0\",\"md5\":\"{$hash}\"}";
+    }
+}
